be099090992cf625d1a5993f12347d16963101dd
Navomita <navomitamaity2002@gmail.com>
2024-11-17 08:30:11 +0530

    footer

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -2,10 +2,9 @@ import React from "react";
 
 
 function Footer() {
-  return
-  setup
-    <div></div>
-  )
+  return(
+  
+    
 
     <footer className="bg-slate-900 text-white py-8">
       <div className="container mx-auto px-4">
@@ -105,8 +104,8 @@ function Footer() {
         </div>
       </div>
     </footer>
-  );
- main
+  )
+ 
 }
 
 export default Footer;
